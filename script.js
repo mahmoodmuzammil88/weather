bbd4be1d8ca2e9869e7fd6786bdf9f26b1963e58
mahmoodmuzammil88 <muzammil9676@gmail.com>
2022-12-14 21:23:46 +0530

    new

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ const options = {
 
 const getWeather = (city) => {
   cityName.innerHTML = city;
+
   fetch(
     "https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=" + city,
     options
@@ -31,10 +32,37 @@ const getWeather = (city) => {
     })
     .catch((err) => console.error(err));
 };
+// ---------------------------------------------------------------------------------------------------------
+const getWeather2 = (city2) => {
+  cityName2.innerHTML = city2;
 
+  fetch(
+    "https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=" + city2,
+    options
+  )
+    .then((response) => response.json())
+    .then((response) => {
+      console.log(response);
+      cloud_pct3.innerHTML = response.cloud_pct;
+      temp3.innerHTML = response.temp;
+      feels_like3.innerHTML = response.feels_like;
+      humidity3.innerHTML = response.humidity;
+      min_temp3.innerHTML = response.min_temp;
+      max_temp3.innerHTML = response.max_temp;
+      wind_speed3.innerHTML = response.wind_speed;
+      sunrise3.innerHTML = response.sunrise;
+      sunset3.innerHTML = response.sunset;
+    })
+    .catch((err) => console.error(err));
+};
+// --------------------------------------------------------------------------------------------------------
 submit.addEventListener("click", (e) => {
   e.preventDefault();
   getWeather(city.value);
+  getWeather2(city.value);
+
 });
 
-getWeather("Delhi");
+getWeather("hyderabad");
+getWeather2("Boston");
+
--- script2.js
+++ script2.js
@@ -5,29 +5,8 @@ const options = {
 		'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
 	}
 };
-const getWeather = (city) => {
-    cityName.innerHTML = city;
-fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=' + city, options)
-	.then((response) => response.json())
-	.then((response) => {
-        console.log(response)
-        cloud_pct3.innerHTML = response.cloud_pct
-        temp3.innerHTML = response.temp
-        feels_like3.innerHTML = response.feels_like
-        humidity3.innerHTML = response.humidity
-        min_temp3.innerHTML = response.min_temp
-        max_temp3.innerHTML = response.max_temp
-        wind_speed3.innerHTML = response.wind_speed
-        wind_degrees3.innerHTML = response.wind_degrees
-        sunrise3.innerHTML = response.sunrise
-        sunset3.innerHTML = response.sunset
-    })
-    .catch((err) => console.error(err));
-}
-submit.addEventListener("click", (e) => {
-    e.preventDefault();
-    getWeather(city.value);
-  });
-  
-  getWeather("Hyderabad");
-  
+
+fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Delhi', options)
+	.then(response => response.json())
+	.then(response => console.log(response))
+	.catch(err => console.error(err));
